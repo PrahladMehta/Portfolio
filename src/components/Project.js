9d65c508d6b666fcfc9c92885e3c0f4b6e77df0f
PrahladMehta <pankajmehta4561@gmail.com>
2024-03-09 11:29:02 +0530

    project add

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,23 +9,34 @@ const Project = function ({ setSelect }) {
         {" "}
         <div className="w-32 h-[6px] bg-[#4770FF] rounded-2xl"></div>
         <h1 className="text-5xl text-[#3A395E] font-bold mt-10">My Projects</h1>
-        <div className="w-[800px] mx-auto">
-          <div className=" mt-32 w-[450px] rounded-2xl shadow-lg hover:scale-105  text-[#6E6D8F] hover:shadow-xl mx-10  cursor-pointer group duration-700">
-            <img src={data[0].img} className="w-full h-[350px]" />
+        <div className="w-full mx-auto flex flex-wrap justify-center">
+          {data.map((val) => {
+            return (
+              <div key={val.id}>
+                <a href={val.gitlink}>
+                  <div className=" mt-32  md:w-[450px]  w-[250px] rounded-2xl shadow-lg hover:scale-105  text-[#6E6D8F] hover:shadow-xl mx-10  cursor-pointer group duration-700">
+                    <img
+                      src={val.img}
+                      className="w-full h-[150px] md:h-[350px]"
+                    />
 
-            <div className="flex flex-col justify-between gap-y-3 p-7 h-[150px] ">
-              <h1 className="text-xl font-bold ">{data[0].title}</h1>
+                    <div className="flex flex-col justify-between gap-y-3 p-7 h-[150px] ">
+                      <h1 className="text-xl font-bold ">{val.title}</h1>
 
-              <div className="flex gap-3  items-center ">
-                <p className="text-blue-600 font-semibold  text-lg   ">
-                  View project{" "}
-                </p>
-                <div className="p-2 rounded-lg bg-blue-100 group-hover:bg-blue-200   text-blue-600 duration-700">
-                  <FaGreaterThan />
-                </div>
+                      <div className="flex gap-3  items-center ">
+                        <p className="text-blue-600 font-semibold  text-lg   ">
+                          View project
+                        </p>
+                        <div className="p-2 rounded-lg bg-blue-100 group-hover:bg-blue-200   text-blue-600 duration-700">
+                          <FaGreaterThan />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- src/components/Projectdata.js
+++ src/components/Projectdata.js
@@ -1,23 +1,24 @@
-import image from "./image/demon.png";
+import image from "./image/Apple-Weather-app.jpg";
+import image2 from "./image/Razorpay.jpg"
+import image3 from "./image/testomonial.jpg"
 const data = [
   {
     id: 1,
     gitlink: "https://github.com/PrahladMehta/Weather-APP",
-    outputlink: "https://weatherapplicationbyprahlad22.netlify.app/",
-    title: "WEATHER APPLICATION",
-    techuse: ["HTML", "CSS", "JAVA SCRIPT", "OPEN WEATHER APPLICATION"],
-    des: "Created a user-friendly weather application using HTML, CSS, and JavaScript.Integrated the OpenWeatherMap API to provide real-time weather data for any location.",
+    title: "WEATHER APPLICATION",  
     img: image,
   },
   {
     id: 2,
     gitlink: "https://github.com/PrahladMehta/Razorpay-Clone",
-    outputlink: "https://razorpayclonebyme.netlify.app/",
+    title:"RAZORPYA-CLONE",
+    img:image2
   },
   {
     id: 3,
     gitlink: "https://github.com/PrahladMehta/Testimonial",
-    outputlink: "https://testomonialproject.netlify.app/",
+    title:"Testiomnial",
+    img:image3
   },
 ];
 
